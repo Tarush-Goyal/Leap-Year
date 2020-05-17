8db18a3a1edb74c39e3f8c26a296b26b7e1a4d2a
tarush <tarush@192.168.66.9>
2020-05-17 20:44:06 +0530

    Added check for following use case: Any year divisible by 100

--- Technical Assessment/src/codingDojo/LeapYear.java
+++ Technical Assessment/src/codingDojo/LeapYear.java
@@ -2,27 +2,99 @@ package codingDojo;
 import java.util.*;
 
 public class LeapYear {
+	
+	int isAYear(String str) {
+		try {
+			int year=Integer.parseInt(str);	
+			System.out.println("Year entered: " + year);
+			return year;
+		}
+		
+		catch(NumberFormatException ex){
+			return 0;
+		}
+			
+	}
+	
+	boolean isDivisibleBy100(int year) {
+		if(year%100==0) {
+			return true;
+		}
+		return false;
+	}
+	
+	boolean isDivisibleBy400(int year) {
+		if(year%400==0) {
+			return true;
+		}
+		return false;
+	}
+	
+	boolean isLessthan1582(int year) {	
+		if(year<=1582) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	boolean isDivisibleBy4(int year){
+		if(year%4==0) {
+			return true;
+		}
+		return false;
+	}
+	
+	boolean isLeapYear(String str) {
+		
+		int year=isAYear(str);
+		
+		if(year>0){
+
+			if(isDivisibleBy4(year) && isLessthan1582(year)){
+				return true;
+			}
+			
+			if(isDivisibleBy400(year)) {
+				return true;
+			}
+			
+			if(isDivisibleBy100(year) && !isDivisibleBy400(year)) {
+				return false;
+			}
+			
+			
+			return false;
+			
+		}
+		else{
+				return false;
+			}
+		
+	}
 
 	public static void main(String[] args) {
 
+		LeapYear ly= new LeapYear();
 		Scanner input=new Scanner(System.in);
-		System.out.print("Enter a number: ");
+		System.out.print("Enter a Year: ");
 		String str=input.nextLine();
-
-		try {
-			int num=Integer.parseInt(str);	
-			System.out.println("Year entered: "+num);
 			
-			if(num<=1582 && num%4==0){
-				System.out.println(num+" is a Leap Year");
+			boolean result=ly.isLeapYear(str);
+			
+			if(result==true) {
+				System.out.println(str + " is a Leap Year");
 			}
+			
+			else{
+				System.out.println(str + " is not a Leap Year");
+			}
+			
 		}
-		catch(NumberFormatException ex) {
-			System.out.println("Not a number");
-		}
+		
 		
 
 
 	}
 
-}
+
