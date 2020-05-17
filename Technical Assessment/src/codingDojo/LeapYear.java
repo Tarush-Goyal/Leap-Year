1e2680c074ac21687721b3a0e1628e2a0ecd5d59
tarush <tarush@192.168.66.9>
2020-05-17 17:10:36 +0530

    Test Case 1: Input not a number

--- Technical Assessment/src/codingDojo/LeapYear.java
+++ Technical Assessment/src/codingDojo/LeapYear.java
@@ -1,9 +1,22 @@
 package codingDojo;
+import java.util.*;
 
 public class LeapYear {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+
+		Scanner input=new Scanner(System.in);
+		String str=input.nextLine();
+
+		try {
+			int num=Integer.parseInt(str);	
+			System.out.println(num);
+		}
+		catch(NumberFormatException ex) {
+			System.out.println("Not a number");
+		}
+		
+
 
 	}
 
