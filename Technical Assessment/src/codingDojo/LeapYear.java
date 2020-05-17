d730bac7093ceaf19546ad7f73e4211a314dd7ea
tarush <tarush@192.168.66.9>
2020-05-17 20:04:31 +0530

    Created methods for use case testing

--- Technical Assessment/src/codingDojo/LeapYear.java
+++ Technical Assessment/src/codingDojo/LeapYear.java
@@ -2,23 +2,55 @@ package codingDojo;
 import java.util.*;
 
 public class LeapYear {
+	
+	boolean isLessthan1582(int year) {	
+		if(year<=1582) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	boolean isDivisibleBy4(int year){
+		if(year%4==0) {
+			return true;
+		}
+		return false;
+	}
+	
+	boolean isLeapYear(int year) {
+		if(isDivisibleBy4(year) && isLessthan1582(year)){
+			return true;
+		}
+		return false;
+	}
 
 	public static void main(String[] args) {
 
+		LeapYear ly= new LeapYear();
 		Scanner input=new Scanner(System.in);
-		System.out.print("Enter a number: ");
+		System.out.print("Enter a Year: ");
 		String str=input.nextLine();
 
 		try {
-			int num=Integer.parseInt(str);	
-			System.out.println("Year entered: "+num);
+			int year=Integer.parseInt(str);	
+			System.out.println("Year entered: " + year);
+			boolean result=ly.isLeapYear(year);
 			
-			if(num<=1582 && num%4==0){
-				System.out.println(num+" is a Leap Year");
+			if(result==true) {
+				System.out.println(year + " is a Leap Year");
 			}
+			
+			else {
+				System.out.println(year + " is not a Leap Year");
+			}
+			
+			
+			
+			
 		}
 		catch(NumberFormatException ex) {
-			System.out.println("Not a number");
+			System.out.println("Not a valid Year");
 		}
 		
 
